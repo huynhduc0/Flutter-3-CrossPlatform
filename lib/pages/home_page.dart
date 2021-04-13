d10c59a5ac22a2f450a73f16497a72c85a3415a9
huynhduc0 <nvngoc2409@gmail.com>
2021-04-13 21:56:17 +0700

    add skeleton loading

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,6 +9,7 @@ import 'package:flutter_food_ordering/pages/user_profile.dart';
 import 'package:flutter_food_ordering/widgets/cart_bottom_sheet.dart';
 import 'package:flutter_food_ordering/widgets/food_card.dart';
 import 'package:provider/provider.dart';
+import 'package:skeleton_text/skeleton_text.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -17,17 +18,27 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int value = 0;
+  bool loading = true;
 
   Future<FoodModel> foodModels;
   Future<FoodCategoryModel> foodCategories;
 
   Future<FoodModel> fetchAllFoods() async {
+    setState(() {
+      foodModels = null;
+    });
     var dio = Dio();
     dio.options.connectTimeout = 5000;
     print('lo con chiêm');
     try {
+      this.setState(() {
+        loading = true;
+      });
       // var response = await dio.get('$BASE_URL/api/foods');
       var response = await dio.get('$BASE_URL/api/food');
+      this.setState(() {
+        loading = false;
+      });
       return FoodModel.fromJson(response.data);
     } catch (e) {
       if (e is DioError) {
@@ -41,6 +52,9 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<FoodModel> fetchFoodByCate(int categoryId) async {
+    setState(() {
+      foodModels =  Future<FoodModel>(null);
+    });
     var dio = Dio();
     dio.options.connectTimeout = 5000;
     try {
@@ -168,7 +182,7 @@ class _MyHomePageState extends State<MyHomePage> {
           if (snapshot.hasData) {
             return ListView(
                 scrollDirection: Axis.horizontal,
-                  physics: BouncingScrollPhysics(),
+                physics: BouncingScrollPhysics(),
                 children: snapshot.data.categories
                     .map(
                       (category) => Padding(
@@ -176,13 +190,21 @@ class _MyHomePageState extends State<MyHomePage> {
                         child: ChoiceChip(
                           selectedColor: mainColor,
                           labelStyle: TextStyle(
-                              color: value ==  category.id? Colors.white : Colors.black),
+                              color: value == category.id
+                                  ? Colors.white
+                                  : Colors.black),
                           label: Text(category.name),
-                          selected: value ==  category.id,
+                          selected: value == category.id,
                           onSelected: (selected) {
                             setState(() {
-                              foodModels = category.id == 0? fetchAllFoods():fetchFoodByCate(category.id);
-                              value =  category.id;
+                              value = category.id;
+                              foodModels = null;
+                            });
+                            setState(() {
+                              value = category.id;
+                              foodModels = category.id == 0
+                                  ? fetchAllFoods()
+                                  : fetchFoodByCate(category.id);
                             });
                           },
                         ),
@@ -234,16 +256,151 @@ class _MyHomePageState extends State<MyHomePage> {
               crossAxisSpacing: 4,
               crossAxisCount: 2,
               physics: BouncingScrollPhysics(),
-              children: snapshot.data.foods.length >0? snapshot.data.foods.map((food) {
-                return FoodCard(food);
-              }).toList():
-              [Text("Nothing to show")],
+              children: snapshot.data.foods.length > 0
+                  ? snapshot.data.foods.map((food) {
+                      return FoodCard(food);
+                    }).toList()
+                  : [Text("Nothing to show")],
             );
           } else if (snapshot.hasError) {
             return Center(child: Text(snapshot.error.toString()));
           }
-          return Center(child: CircularProgressIndicator());
+          // return Center(child: CircularProgressIndicator());
+          return GridView.count(
+            childAspectRatio: 0.65,
+            mainAxisSpacing: 4,
+            crossAxisSpacing: 4,
+            crossAxisCount: 2,
+            physics: BouncingScrollPhysics(),
+            children: List.generate(4, (index) {
+              return Container(
+                child: Card(
+                  shape: roundedRectangle12,
+                  child: Stack(
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          buildImage(),
+                          buildTitle(),
+                          buildRating(),
+                          buildPriceInfo(),
+                        ],
+                      ),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Container(
+                          padding: EdgeInsets.all(4),
+                          decoration: BoxDecoration(
+                            color: mainColor,
+                            borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(12)),
+                          ),
+                         child: Text("Loading")
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            }),
+          );
         },
+      ),
+    );
+  }
+
+  Widget buildImage() {
+    return SkeletonAnimation(
+      shimmerColor: Colors.grey,
+      borderRadius: BorderRadius.circular(20),
+      shimmerDuration: 1000,
+      child: Container(
+        height: MediaQuery.of(context).size.width / 2.5,
+        child: ClipRRect(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
+        ),
+      ),
+    );
+  }
+
+  Widget buildTitle() {
+    return SkeletonAnimation(
+      shimmerColor: Colors.grey,
+      // borderRadius: BorderRadius.circular(20),
+      shimmerDuration: 1000,
+      child: Container(
+        padding: const EdgeInsets.only(left: 8, right: 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Text(
+              "\t\t\t",
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: titleStyle,
+            ),
+            Text(
+                "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t",
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: infoStyle,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildRating() {
+    return SkeletonAnimation(
+      shimmerColor: Colors.grey,
+      shimmerDuration: 1000,
+      child:  Padding(
+      padding: const EdgeInsets.only(left: 4, right: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(
+                mainAxisSize: MainAxisSize.max,
+                // width:MediaQuery.of(context).size.width / 2.5,
+              ),
+              Text('\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t',),
+        ],
+      ),
+    ),
+    );
+  }
+
+  Widget buildPriceInfo() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8, right: 8, bottom: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SkeletonAnimation(
+              shimmerColor: Colors.grey,
+              shimmerDuration: 1000,
+              child: Text(
+                '\t\t\t\t\t\t\t\t\t\t\t\t\t\t',
+                style: titleStyle,
+                overflow: TextOverflow.clip,
+              )),
+          Card(
+            margin: EdgeInsets.only(right: 0),
+            shape: roundedRectangle4,
+            color: mainColor,
+            child: InkWell(
+              splashColor: Colors.white70,
+              customBorder: roundedRectangle4,
+              child: Icon(Icons.add),
+            ),
+          )
+        ],
       ),
     );
   }
