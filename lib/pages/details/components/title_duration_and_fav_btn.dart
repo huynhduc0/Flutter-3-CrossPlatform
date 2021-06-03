fce4bc263e31cc7d7599711a40841a0f98385692
nvchau <nvchau.dev@gmail.com>
2021-06-03 16:20:07 +0700

    Revert "Merge branch 'main' into chau"
    This reverts commit 9f40d44b6c21af639d35c1dcf4eca93898d311a3, reversing
    changes made to 798c595b5857dede7b90005e8d937f8bee48cc51.

--- lib/pages/details/components/title_duration_and_fav_btn.dart
+++ lib/pages/details/components/title_duration_and_fav_btn.dart
@@ -1,54 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_food_ordering/constants/values.dart';
-import 'package:flutter_food_ordering/model/cart_model.dart';
 import 'package:flutter_food_ordering/model/food_model.dart';
-import 'package:provider/provider.dart';
 
-class TitleDurationAndFabBtn extends StatefulWidget {
+class TitleDurationAndFabBtn extends StatelessWidget {
   const TitleDurationAndFabBtn({
     Key key,
     @required this.food,
-    bool isAdded,
   }) : super(key: key);
 
   final Food food;
 
   @override
-  _TitleDurationAndFabBtnState createState() =>
-      _TitleDurationAndFabBtnState(food);
-}
-
-class _TitleDurationAndFabBtnState extends State<TitleDurationAndFabBtn> {
-  bool isAdded = false;
-  int quantity = 0;
-  final Food food;
-
-  _TitleDurationAndFabBtnState(this.food);
-
-  @override
   Widget build(BuildContext context) {
-    var cart = Provider.of<MyCart>(context);
-    for (int i = 0; i < cart.cartItems.length; i++)
-      if (cart.cartItems[i].food.name == food.name) {
-        print(true);
-        setState(() {
-          this.quantity = cart.cartItems[i].quantity;
-          this.isAdded = true;
-        });
-      }
-    // print(cart.cartItems[i])
-    // print("namek" + cart.cartItems[i].food.name);
-    // print(cart.cartItems.length.toString());
-    cart.items.map((e) {
-      print(e.food.id.toString());
-      print(food.id);
-      if (e.food.id == food.id) {
-        print(true);
-        setState(() {
-          this.isAdded = true;
-        });
-      }
-    });
     return Padding(
       padding: EdgeInsets.all(kDefaultPadding),
       child: Row(
@@ -58,20 +21,20 @@ class _TitleDurationAndFabBtnState extends State<TitleDurationAndFabBtn> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Text(
-                  widget.food.name,
+                  food.name,
                   style: headerStyle,
                 ),
                 SizedBox(height: kDefaultPadding / 2),
-                Text(
-                  '\$${widget.food.price}',
-                  style: titleStyle2.copyWith(fontWeight: FontWeight.bold),
-                  // style: TextStyle(color: kTextLightColor),
-                ),
-                SizedBox(width: kDefaultPadding),
                 Row(
                   children: <Widget>[
                     Text(
-                      "${widget.food.shop.name}",
+                      '\$${food.price}',
+                      style: titleStyle2.copyWith(fontWeight: FontWeight.bold),
+                      // style: TextStyle(color: kTextLightColor),
+                    ),
+                    SizedBox(width: kDefaultPadding),
+                    Text(
+                      "${food.shop.name}",
                       style: TextStyle(color: kTextLightColor),
                     ),
                     SizedBox(width: kDefaultPadding),
@@ -85,86 +48,22 @@ class _TitleDurationAndFabBtnState extends State<TitleDurationAndFabBtn> {
             ),
           ),
           SizedBox(
-            height: 50,
-            width: MediaQuery.of(context).size.width / 3,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                isAdded
-                    ? Padding(
-                        padding: const EdgeInsets.only(right: 8),
-                        child: FlatButton(
-                          // height: 15,
-                          minWidth: 30,
-                          onPressed: () {
-                            // bool isAddSuccess =
-
-                            if (quantity == 1)
-                              Provider.of<MyCart>(context, listen: false)
-                                  .removeAllInCart(food);
-                            else
-                              Provider.of<MyCart>(context, listen: false)
-                                  .decreaseDeItem(CartItem(food: food));
-                            this.setState(() {
-                              isAdded = false;
-                            });
-                            // .addItem(
-                            // CartItem(food: widget.food, quantity: -1));
-                            // if (isAddSuccess) {
-                            // ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                            // } else {}
-                            // ScaffoldMessenger.of(context).removeCurrentSnackBar();
-                            // Navigator.pushReplacementNamed(context, '/dashboard');
-                          },
-                          color: mainColor,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                          child: Icon(
-                            Icons.remove,
-                            size: 25,
-                            color: Colors.white,
-                          ),
-                        ),
-                      )
-                    : Spacer(),
-                isAdded
-                    ? Text(
-                        quantity.toString(),
-                        style: headerStyle,
-                      )
-                    : Spacer(),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8),
-                  child: FlatButton(
-                    minWidth: 30,
-                    onPressed: () {
-                      bool isAddSuccess = Provider.of<MyCart>(context,
-                              listen: false)
-                          .addItem(CartItem(food: widget.food, quantity: 1));
-
-                      if (isAddSuccess) {
-                        // ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                      } else {}
-                      // ScaffoldMessenger.of(context).removeCurrentSnackBar();
-                      // Navigator.pushReplacementNamed(context, '/dashboard');
-                    },
-                    color: mainColor,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Icon(
-                      Icons.add,
-                      size: 25,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
+            height: 64,
+            width: 64,
+            child: FlatButton(
+              onPressed: () {},
+              color: mainColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              child: Icon(
+                Icons.add,
+                size: 28,
+                color: Colors.white,
+              ),
             ),
           )
         ],
       ),
     );
-
-    // addItemToCard() {
   }
 }
